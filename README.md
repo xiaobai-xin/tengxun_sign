834df9a9543bdf5c0dfbcffac8b5c9af72fd39d8
Xiaobai1103 <baitianxin@139.com>
2023-04-22 20:26:09 +0800

    一点点修改~

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # 腾讯课堂自动签到程序
-本人水平一般，写的比较垃圾，但是确实能用<br>
 本代码的主要目的是学习openCV，本人不对使用本程序造成的任何后果负责！<br>
 请务必使用管理员运行，因为点击操作pyautogui.click需要管理员权限，否则无法签到！<br>
 我顺便打包了exe,需要的可以到我的博客下载https://xiaobai1103.cn/2022/04/22/tengxun_sign/<br>
 说明：<br>
 main.py是最开始做的代码，仅具有签到功能<br>
 tengxun_auto.py加入了定时启动腾讯课堂并自动进入相应课程<br>
-效果如下图所示：<br>
-![image](https://user-images.githubusercontent.com/103569755/164979182-0f7fea13-788f-49ee-b145-843b2a559320.png)
 
 注释应该写的挺全的，可以根据需要修改<br>
 修改记录：<br>
--- tengxun_auto.py
+++ tengxun_auto.py
@@ -7,8 +7,8 @@ from time import sleep
 
 def check(path):
     target_button=cv2.imread(path, cv2.IMREAD_GRAYSCALE) 
-    screenshot=pyscreeze.screenshot('C:/pic/screenshot.png')#截图
-    target_screenshot=cv2.imread('C:/pic/screenshot.png', cv2.IMREAD_GRAYSCALE)#cv2读入截屏
+    screenshot=pyscreeze.screenshot('../pic/screenshot.png')#截图
+    target_screenshot=cv2.imread('../pic/screenshot.png', cv2.IMREAD_GRAYSCALE)#cv2读入截屏
     ####获取按钮的宽高####  
     sp=target_button.shape
     button_height=sp[0]
@@ -28,20 +28,20 @@ def click(max_loc,X,Y):
     ###鼠标左键点击所计算的目标点####            
     pyautogui.click(taget_X, taget_Y, button='left') 
 
-print("请选择要签到的课程：1=信息论与编码 2=数字信号处理 3=人工智能")
+print("请选择要签到的课程：1=课程1 2=课程2 3=课程3")
 Lesson=input()
 if Lesson=='1':
-    target_button=cv2.imread('C:/pic/L1.png', cv2.IMREAD_GRAYSCALE) #信息论
+    target_button=cv2.imread('../pic/L1.png', cv2.IMREAD_GRAYSCALE) #信息论
     sp=target_button.shape
     L1_height=sp[0]
     L1_width=sp[1]
 elif Lesson=='2':
-    target_button=cv2.imread('C:/pic/L2.png', cv2.IMREAD_GRAYSCALE) #数字信号
+    target_button=cv2.imread('../pic/L2.png', cv2.IMREAD_GRAYSCALE) #数字信号
     sp=target_button.shape
     L1_height=sp[0]
     L1_width=sp[1]
 elif Lesson=='3':
-    target_button=cv2.imread('C:/pic/L3.png', cv2.IMREAD_GRAYSCALE) #人工智能
+    target_button=cv2.imread('../pic/L3.png', cv2.IMREAD_GRAYSCALE) #人工智能
     sp=target_button.shape
     sp=target_button.shape
     L1_height=sp[0]
@@ -52,19 +52,19 @@ else:
 print("设置签到时间，输入hour,min空格隔开:")
 hour_input,min_input =map(int,input().split())
 count=0
-target_button=cv2.imread('C:/pic/in_class.png', cv2.IMREAD_GRAYSCALE) #进入课堂按钮
+target_button=cv2.imread('../pic/in_class.png', cv2.IMREAD_GRAYSCALE) #进入课堂按钮
 sp=target_button.shape
 in_height=sp[0]
 in_width=sp[1]
-target_button=cv2.imread('C:/pic/button.png', cv2.IMREAD_GRAYSCALE) #签到按钮
+target_button=cv2.imread('../pic/button.png', cv2.IMREAD_GRAYSCALE) #签到按钮
 sp=target_button.shape
 sign_height=sp[0]
 sign_width=sp[1]
-target_button=cv2.imread('C:/pic/log.png', cv2.IMREAD_GRAYSCALE) #登录
+target_button=cv2.imread('../pic/log.png', cv2.IMREAD_GRAYSCALE) #登录
 sp=target_button.shape
 log_height=sp[0]
 log_width=sp[1]
-target_button=cv2.imread('C:/pic/right.png', cv2.IMREAD_GRAYSCALE) #右箭头
+target_button=cv2.imread('../pic/right.png', cv2.IMREAD_GRAYSCALE) #右箭头
 sp=target_button.shape
 right_height=sp[0]
 right_width=sp[1]
@@ -73,26 +73,26 @@ while 1:
     hour = datetime.datetime.now().hour
     min = datetime.datetime.now().minute
     if (min>=min_input)&(hour==hour_input):
-        subprocess.Popen('C:\\Program Files (x86)\\Tencent\\TXEDU\\2.0.1.54\\bin\\TXEDU.exe')#启动腾讯课堂
+        subprocess.Popen('..\\Program Files (x86)\\Tencent\\TXEDU\\2.0.1.54\\bin\\TXEDU.exe')#启动腾讯课堂
         sleep(10)
-        res=check('C:/pic/log.png')
+        res=check('../pic/log.png')
         if res:
             click(res,log_width,log_height)#登录
             print("已经登录")
             sleep(10)
             while(1):
-                res=check('C:/pic/L1.png')#查找课程L1
+                res=check('../pic/L1.png')#查找课程L1
                 if res:
                     click(res,L1_width,L1_height)#点击打开课程
-                    res=check('C:/pic/in_class.png')
+                    res=check('../pic/in_class.png')
                     click(res,in_width,in_height)#点击进入直播
                     print("已经打开课程，接下来将为您自动签到")
                     while 1:
-                        res=check('C:/pic/button.png')#查找签到按钮
+                        res=check('../pic/button.png')#查找签到按钮
                         if res:
                             click(res,sign_width,sign_height)#点击签到
                             sleep(3)
-                            res=check('C:/pic/button.png')#再次检查签到按钮是否存在
+                            res=check('../pic/button.png')#再次检查签到按钮是否存在
                             if res:
                                 print("警告：签到可能失败，请及时确认！")
                             else:
@@ -100,7 +100,7 @@ while 1:
                                 print("已经完成",count,"次签到")
                         sleep(5)
                 else :
-                    res=check('C:/pic/right.png')
+                    res=check('../pic/right.png')
                     click(res,right_width,right_height)#下一页
                     sleep(5)
 
